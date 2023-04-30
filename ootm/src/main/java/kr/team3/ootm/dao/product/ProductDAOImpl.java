60e824829cf25742198a21583209f4197efa17e7
Hoo <adobephotoshop@naver.com>
2023-04-30 09:03:19 +0900

    주석추가

--- ootm/src/main/java/kr/team3/ootm/dao/product/ProductDAOImpl.java
+++ ootm/src/main/java/kr/team3/ootm/dao/product/ProductDAOImpl.java
@@ -50,6 +50,17 @@ public class ProductDAOImpl implements ProductDAO{
 
 	@Override
 	public ArrayList<ProductDTO> selectBestProduct(int amount) {
+		//spring jdbc를 연동한다면
+		//dao상위하위서비스상위하위 arrayList 들어간부분 list로 바구고
+		//sql = "select * from product order by product_sale_rate desc limit ?";
+		//return template.queryForObject(sql, 
+		//new Object[] {amount}, new ProductRowMapper());
+		//밑에 잇는 new도 비슷한방법으로 order by regdate
+		//아직 프로덕트에 의미잇는ㄷ ㅔ이터들이 없기에 
+		//대충 포문으로 랜덤으로 넣어서 리턴중.,
+		
+		
+		
 		// db연동 없이 일단 리스트 amount개
 		ArrayList<ProductDTO> result = new ArrayList<ProductDTO>();
 		Random r = new Random();
